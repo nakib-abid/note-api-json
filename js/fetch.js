5bbfbb6be998c2b7cb6d111670c47bd808a04a07
nakib-abid <nakib.abid@gmail.com>
2024-10-01 21:55:42 +0600

    API-JSON

---
+++ js/fetch.js
@@ -0,0 +1,13 @@
+// json placeholder google search
+// url টি একটি variable এ রাখা যায় 
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json()) // .json is not similar but close to json.parse
+//       .then(json => console.log(json))
+
+function loadData(){
+    const url = 'https://jsonplaceholder.typicode.com/todos/1'
+    fetch(url)
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
